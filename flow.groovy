508b223691be53ff9b02812ab5f2a15d4a0011ea
scor <omgbebebe@gmail.com>
2015-07-13 12:45:17 +0300

    initial build skeleton

--- flow.groovy
+++ flow.groovy
@@ -12,11 +12,27 @@ node {
 			submoduleCfg: [],
 			userRemoteConfigs: [[url: "$GITLAB_DEFAULT_SSH_URL" + '/' + "$PROJECT_NAME"]]]
 
-	stage 'Deploy'
-	echo 'deploying'
+	echo('clonning jenkins-common repo')
+	git branch: "$JENKINS_COMMON_BRANCH"
+		, changelog: false
+		, poll: false
+		, url: "$GITLAB_DEFAULT_SSH_URL/$JENKINS_COMMON_REPO_NAME"
 
-	stage 'ITests'
-	echo 'testing'
+	echo('build via jenkins-common script')
+	sh "$WORKSPACE/$JENKINS_COMMON_REPO_NAME/scripts/build/get_and_run_cicd_build.sh"
+
+	echo('pack artifacts')
+	archive '**/target/*.jar,**/target/*.tar.gz, down-stream-job.properties'
+
+	if($PERFORM_DEPLOY) {
+		stage 'Deploy'
+		echo 'deploying'
+	}
+
+	if($PERFORM_TESTS) {
+		stage 'ITests'
+		echo 'testing'
+	}
 
 	stage 'UnDeploy'
 	echo 'cleaning'
